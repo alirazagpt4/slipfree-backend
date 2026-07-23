fe4010820f8b1efe44982dcfea1bb1a2d5433768
alirazagpt4 <ali.4aug24webgpt@gmail.com>
2026-07-24 01:50:24 +0500

    first slipfree-backend commit

--- app/app.js
+++ app/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import morgan from 'morgan';
 import invoiceRoutes from '../routes/invoice.routes.js';
 
 const app = express();
 
 app.use(express.json());
+app.use(morgan('dev'));
 app.use('/api/v1/receipts', invoiceRoutes);
 
 app.get('/', (req, res) => {
